1015bee616aaccecb6a900fd47c25604a97d7d4a
Walter <wg904093@gmail.com>
2023-04-06 20:59:11 -0300

    navbar adaptado a dispositivos mobiles

--- src/components/navbar/Navbar.element.js
+++ src/components/navbar/Navbar.element.js
@@ -12,7 +12,7 @@ export const NavContainer = styled.nav`
     position: sticky;
     z-index:99;
     background-color: var(--color_black_primary);
-    justify-content: space-between;
+    justify-content: center;
 `;
 
 // component for Logo page
@@ -32,13 +32,52 @@ export const Redes = styled(RedLink)`
         ${SvgProps}
     }
 `
+
+// container for nav
+
+export const NavWrapper = styled.div`
+    ${FlexContainer}
+    justify-content: space-between;
+    width: 100%;
+    overflow: hidden;
+
+    @media screen and (max-width: 1000px) {
+        padding: 10px;
+    }
+`
+
+// container for links and redes
+
+export const BarContainer = styled.div`
+    ${FlexContainer}
+    transition: all 1s ease;
+    @media screen and (max-width: 1000px) {
+        position: absolute;
+        left: ${({active}) => active ? '0' : '-400%'};
+        top: ${({active}) => active ? '60px' : '-400%'};
+        transition: all 1s ease;
+        flex-flow: column;
+        justify-content: space-around;
+        width: 100%;
+        height: 80vh;
+        background-color: var(--color_black_primary);
+        border-bottom-right-radius: 50%;
+    }
+`
 // components for the table 
 
 export const NavUl = styled.ul`
     margin: 0;
+    margin-right: 50px;
     height: 30px;
     ${FlexContainer}
     justify-content: space-around;
+
+    @media screen and (max-width: 1000px) {
+        flex-flow: column;
+        width: 100%;
+        height: 60%;
+    }
 ` 
 
 export const NavLi = styled.li`
@@ -70,5 +109,16 @@ export const RedesContainer = styled.div`
 
     .redes{
         margin-right: 1.2rem;
+    }
+`
+
+// container for icon mobile 
+
+export const IconMobile = styled.div`
+    display: none;
+    ${SvgProps}
+
+    @media screen and (max-width: 1000px) {
+        display: flex;
     }
 `
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,45 @@
-import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { LogoContainer, NavContainer, NavLi, NavLink, NavUl, Redes, RedesContainer } from "./Navbar.element";
+import { FaGithub, FaInstagram, FaLinkedinIn,FaBars, FaTimes } from "react-icons/fa";
+import { BarContainer, IconMobile, LogoContainer, NavContainer, NavLi, NavLink, NavUl, NavWrapper, Redes, RedesContainer } from "./Navbar.element";
+import { useState } from "react";
 
 export const Navbar = () => {
+
+  const [menuMobilShow, setMenuMobilShow] = useState(false)
   return (
     <NavContainer>
-        <LogoContainer>
-          
+
+      <NavWrapper>
+
+        <LogoContainer>        
           <Redes href="https://github.com/walter-doctype" target="_blank">
             <FaGithub/>
           </Redes>
-
         </LogoContainer>
 
+        <IconMobile onClick={() => setMenuMobilShow(!menuMobilShow)}>
+          {
+            menuMobilShow
+              ? <FaTimes/>
+              : <FaBars/>
+          }
+        </IconMobile>
 
-        <NavUl>
-          <NavLi><NavLink href="#">Home</NavLink></NavLi>
-          <NavLi><NavLink href="#">About me</NavLink></NavLi>
-          <NavLi><NavLink href="#">Skills</NavLink></NavLi>
-          <NavLi><NavLink href="#">Contact me</NavLink></NavLi>
-        </NavUl>
+        <BarContainer active={menuMobilShow}>
+          <NavUl>
+            <NavLi onClick={() => setMenuMobilShow(!menuMobilShow)}><NavLink href="#">Home</NavLink></NavLi>
+            <NavLi onClick={() => setMenuMobilShow(!menuMobilShow)}><NavLink href="#">About me</NavLink></NavLi>
+            <NavLi onClick={() => setMenuMobilShow(!menuMobilShow)}><NavLink href="#">Skills</NavLink></NavLi>
+            <NavLi onClick={() => setMenuMobilShow(!menuMobilShow)}><NavLink href="#">Contact me</NavLink></NavLi>
+          </NavUl>
 
-        <RedesContainer>
-          <Redes className="redes"><FaInstagram/></Redes>
-          <Redes className="redes"><FaGithub/></Redes>
-          <Redes className="redes"><FaLinkedinIn/></Redes>
-        </RedesContainer>
+          <RedesContainer>
+            <Redes className="redes"><FaInstagram/></Redes>
+            <Redes className="redes"><FaGithub/></Redes>
+            <Redes className="redes"><FaLinkedinIn/></Redes>
+          </RedesContainer>
+        </BarContainer>
+      </NavWrapper>
+
     </NavContainer>
 
     
